f672cbb748e3a829bbdb5b1c3adeaa9ca9104845
Bruno-Rizak <brunorizak@gmail.com>
2023-04-17 14:47:17 -0300

    update

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+	<head><title>Login</title></head>	
+	<body>
+
+	<h1>SISTEMA DE LOGIN</h1>
+		<form action="usuario.php" method="POST">
+			Usuario <input type="text" name="login"/> <br>
+			Senha <input type="password" name="senha"/> <br>
+			<input type="submit" value="Logar"/> <br>
+	        </form>
+</body>
+</html>
